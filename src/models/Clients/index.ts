24b7e0c5778b7d0f66e49b1db08aacddd68bcd97
Chrigor Eduardo <chrigor.edu3@gmail.com>
2021-06-06 11:16:01 -0300

    Criando model de client

---
+++ src/models/Clients/index.ts
@@ -0,0 +1,45 @@
+import { Document, Schema, model } from "mongoose";
+import EncryptPassword from "../../utils/EncryptPassword";
+
+const ObjectId = Schema.Types.ObjectId;
+
+import { ClientsInterface, AuthInterface } from "./IClients";
+
+export interface ClientModelInterface extends ClientsInterface, Document {}
+
+const ClientSchema: Schema = new Schema({
+  name: String,
+  blocked: Boolean,
+  reason: String,
+  auth: {
+    email: String,
+    password: String,
+  },
+  plano: {
+    type: ObjectId,
+    default: null,
+  },
+  clients: Array,
+  createdAt: Date,
+  updateAt: Date,
+});
+
+const encryptAuth = async (auth: AuthInterface): Promise<AuthInterface> => {
+  auth.password = await EncryptPassword(auth.password);
+
+  return auth;
+};
+
+ClientSchema.pre<ClientsInterface>("save", async function (next) {
+  this.auth = await encryptAuth(this.auth);
+  next();
+});
+
+ClientSchema.pre<ClientsInterface>("updateOne", async function (next) {
+  const { auth = {} } = this._update;
+
+  this.auth = await encryptAuth(auth);
+  next();
+});
+
+export default model<ClientsInterface>("Client", ClientSchema);
