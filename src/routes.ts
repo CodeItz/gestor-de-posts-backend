a23475d5b0c01390927ece98f47ac19c9b9ecb3d
Chrigor Eduardo <chrigor.edu3@gmail.com>
2021-06-06 11:19:33 -0300

    add rotas no arquivo routes

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,10 @@ import SubClientController from "./controllers/SubClient";
 import createSubClient from "./validations/SubClients/create";
 import updateSubClient from "./validations/SubClients/create";
 
+
+import createClient from "./validations/Clients/create";
+import ClientController from "./controllers/Client";
+
 const routes = Router();
 
 routes.get("/", (req: Request, res: Response) => {
@@ -17,6 +21,10 @@ routes.get("/subclients", SubClientController.index);
 routes.post("/subclients", createSubClient, SubClientController.store);
 routes.put("/subclients/:id", updateSubClient, SubClientController.update);
 routes.get("/subclients/:id", SubClientController.show);
+
+routes.get("/clients", ClientController.index);
+routes.post("/clients", createClient, ClientController.store);
+routes.get("/clients/:id", ClientController.show);
 
 const uploader = multer({
   storage: multer.memoryStorage(),
