f8f8ffff7c4b8377d7366a80868c61e949255478
Chrigor Eduardo <chrigor.edu3@gmail.com>
2021-06-20 17:15:56 -0300

    add hour to post

--- src/utils/Encrypt.ts
+++ src/utils/Encrypt.ts
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 export async function encryptString(text: string) {
   const SALTS = 8;
@@ -7,4 +8,29 @@ export async function encryptString(text: string) {
 
 export async function compare(text: string, text2: string) {
   return await bcrypt.compare(text, text2);
+}
+
+const CONFIG_CRYPTO = {
+  algorithm: "aes-256-ctr",
+  password: "d6F3Efeq",
+};
+
+export function encrypt(text: string) {
+  var cipher = crypto.createCipher(
+    CONFIG_CRYPTO.algorithm,
+    CONFIG_CRYPTO.password
+  );
+  var crypted = cipher.update(text, "utf8", "hex");
+  crypted += cipher.final("hex");
+  return crypted;
+}
+
+export function decrypt(text: string) {
+  var decipher = crypto.createDecipher(
+    CONFIG_CRYPTO.algorithm,
+    CONFIG_CRYPTO.password
+  );
+  var dec = decipher.update(text, "hex", "utf8");
+  dec += decipher.final("utf8");
+  return dec;
 }
--- src/validations/Post/create.ts
+++ src/validations/Post/create.ts
@@ -4,6 +4,7 @@ import * as Yup from "yup";
 const schema = Yup.object().shape({
   subClientId: Yup.string().required().min(3),
   photoURL: Yup.string().required().min(3),
+  hourScheduled: Yup.string().required().min(5),
   description: Yup.string(),
   dateScheduled: Yup.date().required(),
 });
